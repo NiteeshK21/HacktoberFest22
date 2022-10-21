a8a732da203890e46d48ba39a791c03bed7a876e
Shaz'e <shazinabbas_m@srmap.edu.in>
2022-10-21 13:07:25 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -15,27 +15,6 @@ Bill_label=tk.Label(win,text="WELCOME TO HOTEL NSR",bg="black",fg="Snow",font=("
 Bill_label.grid(row=0,column=0,sticky="NSWE")
 rn = 100
 rntype=''
-#Kshitij
-#LinkedList
-class Node:
-   def __init__(self, dataval=None):
-      self.dataval = dataval
-      self.nextval = None
-
-class SLinkedList:
-   def __init__(self):
-      self.headval = None
-
-list1 = SLinkedList()
-list1.headval = Node("Mon")
-e2 = Node("Tue")
-e3 = Node("Wed")
-# Link first Node to second node
-list1.headval.nextval = e2
-
-# Link second Node to third node
-e2.nextval = e3
-#End
 
 def start(): #function that starts the whole program system. 
     app=tk.Tk()
